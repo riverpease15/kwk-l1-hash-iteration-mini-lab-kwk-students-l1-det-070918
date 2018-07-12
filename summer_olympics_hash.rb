5f9d2c797065b93bd6b10010ed0a6d4cf1c2e710
River Pease <riverpease15@gmail.com>
2018-07-12 15:40:56 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -6,12 +6,13 @@ end
 
 def add_a_key_value_pair
   # Implement this method so that it adds a key value pair to the hash created in create_olympics_hash
-  create_olympics_hash[:Atlanta] =
+  create_olympics_hash[:Atlanta] = 1996
 end
 
 def iterate_through_hash
   # Implement this method so that it iterates over the hash created in add_a_key_value_pair
   # and puts each key value pair within the phrase "The _____ summer olympics took place in _____."
+  
 end
 
 def iterate_through_keys
